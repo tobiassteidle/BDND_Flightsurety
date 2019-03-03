f7d1d9ab0ca7615b146710cd6350f6467cf0682b
Tobias Steidle <tobias.steidle@softwaredev.de>
2019-03-03 14:16:19 +0100

    feat: implemented dummy flight register callback.

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -9,14 +9,71 @@ import './flightsurety.css';
     let result = null;
 
     let contract = new Contract('localhost', () => {
+        let self = this;
 
         // Read transaction
         contract.isOperational((error, result) => {
-            console.log(error,result);
-            display('Operational Status', 'Check if contract is operational', [ { label: 'Operational Status', error: error, value: result} ]);
-        });
-    
+            console.log(error ? error : "", "Operational Status: " + result);
 
+            let flights = [
+                {
+                    time: "09:15",
+                    target: "Aberdeen",
+                    fn: "BD674",
+                    status: "Delayed"
+                },
+                {
+                    time: "09:45",
+                    target: "Newcastle",
+                    fn: "BA1326",
+                    status: "Cancelled"
+                },
+                {
+                    time: "09:55",
+                    target: "Glasgow",
+                    fn: "BA1476",
+                    status: "Cancelled"
+                },
+                {
+                    time: "09:55",
+                    target: "Durham Tees",
+                    fn: "GF5232",
+                    status: "On Time"
+                },
+                {
+                    time: "10:05",
+                    target: "Dublin",
+                    fn: "AA7991",
+                    status: "Delayed"
+                },
+                {
+                    time: "10:10",
+                    target: "Shannon",
+                    fn: "AA8017",
+                    status: "Cancelled"
+                },
+                {
+                    time: "10:35",
+                    target: "Edinburgh",
+                    fn: "BA1447",
+                    status: "On Time"
+                }
+            ];
+
+
+           // flights = null;
+
+            function registerFlight(flight){
+                contract.registerFlight(flight, () => {
+                    console.log("DANONE");
+                })
+            }
+
+
+            displayFlightplan( 'Operational Status', 'Check if contract is operational', flights, [ { label: 'Operational Status', error: error, value: result} ], registerFlight);
+        });
+
+/*
         // User-submitted transaction
         DOM.elid('submit-oracle').addEventListener('click', () => {
             let flight = DOM.elid('flight-number').value;
@@ -25,26 +82,83 @@ import './flightsurety.css';
                 display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
             });
         })
-    
+*/
+        contract.oracleReport(result => {
+           console.log(JSON.stringify(result));
+        });
+
+        contract.flightStatusInfo(result => {
+            console.log("*** VERIFIED: " + JSON.stringify(result) + " ****");
+        });
+
+
+
     });
-    
 
 })();
 
 
-function display(title, description, results) {
+function displayFlightplan(title, description, flights, status, registerFlightCallback) {
     let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h2(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className:'row'}));
+    displayDiv.innerHTML = "";
+
+    // Available Flights
+    let sectionFlightPlan = DOM.section();
+    sectionFlightPlan.appendChild(DOM.h2("Flightplan"));
+
+    if(flights !== null){
+        sectionFlightPlan.appendChild(DOM.h5("Currently available flights"));
+
+        flights.map((flight) => {
+            let row = sectionFlightPlan.appendChild(DOM.div({className:'row'}));
+            row.appendChild(DOM.div({className: 'col-sm-1 field-value'}, flight.time));
+            row.appendChild(DOM.div({className: 'col-sm-1 field-value'}, flight.fn));
+            row.appendChild(DOM.div({className: 'col-sm-2 field-value'}, flight.target));
+            row.appendChild(DOM.div({className: 'col-sm-2 field'}, flight.status));
+
+
+            let buttonInsuring = DOM.button({className: 'btn btn-primary', id: 'dasfsdf'}, "Register insuring");
+            buttonInsuring.addEventListener('click', () => {
+                registerFlightCallback(flight.fn);
+            });
+            row.appendChild(buttonInsuring);
+/*
+
+            time: "09:45",
+              target: "Newcastle",
+              fn: "BA1326",
+              status: "Cancelled"
+  */
+
+            sectionFlightPlan.appendChild(row);
+        })
+
+    } else {
+        sectionFlightPlan.appendChild(DOM.h5("Loading available Flights..."));
+    }
+
+
+    /*
+
+*/
+    /*
+<div class="row top-20">
+      <label class="form">Flight</label> <input type="text" id="flight-number"> <btn class="btn btn-primary" id="submit-oracle">Submit to Oracles</btn>
+    </div>*/
+
+    displayDiv.append(sectionFlightPlan);
+
+    // Operational Status
+    let sectionOperationalStatus = DOM.section();
+    sectionOperationalStatus.appendChild(DOM.h4(title));
+    sectionOperationalStatus.appendChild(DOM.h5(description));
+    status.map((result) => {
+        let row = sectionOperationalStatus.appendChild(DOM.div({className:'row'}));
         row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
         row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
+        sectionOperationalStatus.appendChild(row);
     })
-    displayDiv.append(section);
-
+    displayDiv.append(sectionOperationalStatus);
 }
 
 
